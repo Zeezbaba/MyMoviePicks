2e548b6907f52bdb36f6eeb2d3f66cf82f30f288
Zeezbaba <okelejitimi93@gmail.com>
2025-08-04 04:56:45 +0100

    hotfix: fixed the env file

--- backend/MyMoviePicks/settings.py
+++ backend/MyMoviePicks/settings.py
@@ -101,6 +101,12 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+print("DB_NAME:", os.getenv("DB_NAME"))
+print("DB_USER:", os.getenv("DB_USER"))
+print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
+print("DB_HOST:", os.getenv("DB_HOST"))
+print("DB_PORT:", os.getenv("DB_PORT"))
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
